7a93527b1656df8b24ebff3d7434bae8b9fc63ec
Shihab221 <ashihab205@gmail.com>
2025-05-08 10:45:10 +0600

    models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, default=1)
+    def __str__(self):
+        return self.name
+
+class Size(models.Model):
+    name = models.CharField(max_length=10)  # e.g. S, M, L, XL, 32, 42
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    name = models.CharField(max_length=30)  # e.g. Red, Blue
+    hex_code = models.CharField(max_length=7, blank=True)  # optional
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='product_images/', blank=True)
+    description = models.CharField(max_length=255)
+    detailed_description = models.TextField(max_length=1000, default="detailed description")
+    regular_price = models.DecimalField(max_digits=6, decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    rating = models.DecimalField(max_digits=6, decimal_places=2, default=4.5)
+    sizes = models.ManyToManyField(Size, blank=True)
+    colors = models.ManyToManyField(Color, blank=True)
+    
+    def __str__(self):
+        return self.name
